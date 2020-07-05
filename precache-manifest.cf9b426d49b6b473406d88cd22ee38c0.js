9858fd3605b6d73c848c30a7305fbc60da47c812
AntonyRajRichard <antony.raj@codingmart.com>
2020-07-06 03:52:17 +0530

    Updates

---
+++ precache-manifest.cf9b426d49b6b473406d88cd22ee38c0.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2e03cd94d2bd115e77cb46eef0687df8",
+    "url": "/RootQuotient-Client/index.html"
+  },
+  {
+    "revision": "00b5580ba5364454a268",
+    "url": "/RootQuotient-Client/static/css/2.c27a359e.chunk.css"
+  },
+  {
+    "revision": "8b81528d880d665a070a",
+    "url": "/RootQuotient-Client/static/css/main.fcc0558b.chunk.css"
+  },
+  {
+    "revision": "00b5580ba5364454a268",
+    "url": "/RootQuotient-Client/static/js/2.25eaa1f5.chunk.js"
+  },
+  {
+    "revision": "f4dca002272ba70d739a77376b0079dd",
+    "url": "/RootQuotient-Client/static/js/2.25eaa1f5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8b81528d880d665a070a",
+    "url": "/RootQuotient-Client/static/js/main.81945177.chunk.js"
+  },
+  {
+    "revision": "c2888af84e3cd09864f3",
+    "url": "/RootQuotient-Client/static/js/runtime-main.65f732e0.js"
+  },
+  {
+    "revision": "ed25a86ace7884ed269222f5f4cf17e3",
+    "url": "/RootQuotient-Client/static/media/buttonloader.ed25a86a.gif"
+  },
+  {
+    "revision": "ee0075986785d9206c068f1b7288ae85",
+    "url": "/RootQuotient-Client/static/media/loader.ee007598.gif"
+  }
+]);
