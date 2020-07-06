d8ed5ee14f03e9514d289c16281aa64298397348
AntonyRajRichard <antony.raj@codingmart.com>
2020-07-06 16:49:22 +0530

    build file added to repo

---
+++ build/precache-manifest.4cd87e95c393b53c1fdc3dbace50ff7b.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e9c8a820f296698954bb9197c12f80ba",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3de499a70dce5d7f0915",
+    "url": "/static/css/2.c27a359e.chunk.css"
+  },
+  {
+    "revision": "72a47bf6ae2c8f59492d",
+    "url": "/static/css/main.0b2392a1.chunk.css"
+  },
+  {
+    "revision": "3de499a70dce5d7f0915",
+    "url": "/static/js/2.0630214c.chunk.js"
+  },
+  {
+    "revision": "f4dca002272ba70d739a77376b0079dd",
+    "url": "/static/js/2.0630214c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "72a47bf6ae2c8f59492d",
+    "url": "/static/js/main.9c96d1f4.chunk.js"
+  },
+  {
+    "revision": "400f68a08fe7e360eb09",
+    "url": "/static/js/runtime-main.a80e5bda.js"
+  },
+  {
+    "revision": "ed25a86ace7884ed269222f5f4cf17e3",
+    "url": "/static/media/buttonloader.ed25a86a.gif"
+  },
+  {
+    "revision": "ee0075986785d9206c068f1b7288ae85",
+    "url": "/static/media/loader.ee007598.gif"
+  }
+]);
